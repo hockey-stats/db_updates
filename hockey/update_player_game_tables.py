1edc47305cd0e66718af74861684dcb8c30865d1
Sohraub Pazuki <sohraub.pazuki@arctiq.ca>
2025-09-18 15:10:44 -0400

    moved artifact download to python script

--- hockey/update_player_game_tables.py
+++ hockey/update_player_game_tables.py
@@ -1,5 +1,9 @@
 import os
 import glob
+import requests
+import datetime
+import zipfile
+import json
 from argparse import ArgumentParser
 
 import duckdb
@@ -11,6 +15,57 @@ import polars as pl
 DB_NAME = 'hockey-stats.db'
 
 ########### End Constants ###############
+
+
+def download_database() -> None:
+    """
+    Function to download the artifact containing the .db file from GitHub.
+
+    Looks for the version of the artifact that was most recently updated, assuming that
+    to be the most recent version of the database.
+
+    Expects a GitHub PAT with proper permissions to be available as an environment variable (in
+    this case provided by the GitHub Actions workflow).
+    """
+    url = "https://api.github.com/repos/hockey-stats/db_updates/actions/artifacts"
+    payload = {}
+    headers = {
+        'Authorization': f'Bearer {os.environ["GITHUB_PAT"]}'
+    }
+    output_filename = 'db.zip'
+
+    # Returns a list of every available artifact for the repo
+    response = requests.request("GET", url, headers=headers, data=payload, timeout=10)
+    response_body = json.loads(response.text)
+
+    # Variable to store the most recent updated time for an artifact, for comparison
+    most_recent_update = None
+    download_url = ''
+
+    for artifact in response_body['artifacts']:
+        update_time = datetime.datetime.strptime(artifact['updated_at'], '%Y-%m-%dT%H:%M:%SZ')\
+            .replace(tzinfo=datetime.timezone.utc)
+
+        if most_recent_update is None or most_recent_update or update_time > most_recent_update:
+            most_recent_update = update_time
+            download_url = artifact['archive_download_url']
+
+    if most_recent_update is None:
+        # If no artifact was found, raise an error
+        raise ValueError("No artifact found, exiting...")
+
+    # Downloads the artifact as a zip file
+    dl_response = requests.request("GET", download_url, headers=headers, data=payload, timeout=60)
+    with open(output_filename, 'wb') as fo:
+        fo.write(dl_response.content)
+
+    # And unzip
+    with zipfile.ZipFile(output_filename, 'r') as zip_ref:
+        zip_ref.extractall(os.getcwd())
+
+    print(os.listdir(os.getcwd))
+
+    print('Database download complete')
 
 
 def process_skater_data(path: str, game_id: int) -> pl.DataFrame:
--- hockey/update_tables.py
+++ hockey/update_tables.py
@@ -1,6 +1,11 @@
+import datetime
+import zipfile
+import os
+import json
 from argparse import ArgumentParser
-from datetime import datetime
+
 import duckdb
+import requests
 
 import process_skater_data
 import process_team_data
@@ -14,6 +19,58 @@ DB_NAME = 'hockey-stats.db'
 ########### End Constants ###############
 
 
+def download_database() -> None:
+    """
+    Function to download the artifact containing the .db file from GitHub.
+
+    Looks for the version of the artifact that was most recently updated, assuming that
+    to be the most recent version of the database.
+
+    Expects a GitHub PAT with proper permissions to be available as an environment variable (in
+    this case provided by the GitHub Actions workflow).
+    """
+    url = "https://api.github.com/repos/hockey-stats/db_updates/actions/artifacts"
+    payload = {}
+    headers = {
+        'Authorization': f'Bearer {os.environ["GITHUB_PAT"]}'
+    }
+    output_filename = 'db.zip'
+
+    # Returns a list of every available artifact for the repo
+    response = requests.request("GET", url, headers=headers, data=payload, timeout=10)
+    response_body = json.loads(response.text)
+
+    # Variable to store the most recent updated time for an artifact, for comparison
+    most_recent_update = None
+    download_url = ''
+
+    print(response_body)
+    for artifact in response_body['artifacts']:
+        update_time = datetime.datetime.strptime(artifact['updated_at'], '%Y-%m-%dT%H:%M:%SZ')\
+            .replace(tzinfo=datetime.timezone.utc)
+
+        if most_recent_update is None or most_recent_update or update_time > most_recent_update:
+            most_recent_update = update_time
+            download_url = artifact['archive_download_url']
+
+    if most_recent_update is None:
+        # If no artifact was found, raise an error
+        raise ValueError("No artifact found, exiting...")
+
+    # Downloads the artifact as a zip file
+    dl_response = requests.request("GET", download_url, headers=headers, data=payload, timeout=60)
+    with open(output_filename, 'wb') as fo:
+        fo.write(dl_response.content)
+
+    # And unzip
+    with zipfile.ZipFile(output_filename, 'r') as zip_ref:
+        zip_ref.extractall(os.getcwd())
+
+    print(os.listdir(os.getcwd()))
+
+    print('Database download complete')
+
+
 def main(season: int) -> None:
     """
     This script is designed to be run every morning within a GitHub Actions workflow. 
@@ -23,6 +80,9 @@ def main(season: int) -> None:
 
     :param int season: NHL season for which to pull data
     """
+
+    print('Downloading database artifact...')
+    download_database()
 
     print('Gathering skater data...')
     skater_df = process_skater_data.gather_df(season)
@@ -48,8 +108,8 @@ def main(season: int) -> None:
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-s', '--season', type=int,
-                        default=datetime.now().year - 1 if datetime.now().month < 10 \
-                                else datetime.now().year,
+                        default=datetime.datetime.now().year - 1 if datetime.datetime.now().month < 10 \
+                                else datetime.datetime.now().year,
                         help='Season for which we pull data')
     args = parser.parse_args()
 
