d6e97782c1dcd4e022a7557ca5716f772d1ef246
Sohraub Pazuki <sohraub.pazuki@arctiq.ca>
2025-09-22 12:24:36 -0400

    testing scraping workflow

--- hockey/initialize_player_game_tables.py
+++ hockey/initialize_player_game_tables.py
@@ -1,3 +1,4 @@
+import argparse
 import duckdb
 
 
@@ -13,11 +14,22 @@ skater data that will be updated after every game.
 """
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--preseason', action='store_true', default=False,
+                        help='Enable flag to create tables for preseason data.')
+    args = parser.parse_args()
+
     print('Connecting to database...')
     conn = duckdb.connect(database=DB_NAME, read_only=False)
 
-    conn.execute("""
-                CREATE OR REPLACE TABLE skater_games (
+    skater_table = 'skater_games'
+    goalie_table = 'goalie_games'
+    if args.preseason:
+        skater_table = f'preseason_{skater_table}'
+        goalie_table = f'preseason_{goalie_table}'
+
+    conn.execute(f"""
+                CREATE OR REPLACE TABLE {skater_table} (
                     name VARCHAR,
                     gameID INT,
                     gameDate DATE,
@@ -42,10 +54,10 @@ if __name__ == '__main__':
                  );
                  """)
 
-    print('skater_games table initialized!')
+    print(f'{skater_table} table initialized!')
 
-    conn.execute("""
-                CREATE OR REPLACE TABLE goalie_games (
+    conn.execute(f"""
+                CREATE OR REPLACE TABLE {goalie_table} (
                     name VARCHAR,
                     gameID INT,
                     gameDate DATE,
@@ -58,6 +70,6 @@ if __name__ == '__main__':
                  );
                 """)
 
-    print('goalie_games table initialized!')
+    print(f'{goalie_table} table initialized!')
 
     print('Table initialization complete!')
